46d18087415c1661dc4791ffc8630405bfad59d0
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-14 14:56:02 +0300

    the routes of user have been added

---
+++ routes/user.routes.js
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+
+const userRouter = Router();
+
+userRouter.get('/', (req, res) =>  res.send({title: "Fetch all users"}));
+
+userRouter.get('/:id', (req, res) =>  res.send({title: "GET user details"}));
+
+userRouter.post('/', (req, res) =>  res.send({title: "Create new user"}));
+userRouter.put('/:id', (req, res) =>  res.send({title: "Update all users"}));
+userRouter.delete('/:id', (req, res) =>  res.send({title: "Fetch all users"}));
+
+export default userRouter;
