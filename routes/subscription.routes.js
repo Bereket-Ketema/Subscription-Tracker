95219cc041be33a787e5c5e1dc566f141b244939
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-14 16:17:25 +0300

    the subscription CRUD have been arranged

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,6 +2,13 @@ import {Router} from 'express';
 
 const subscriptionRouter = Router();
 
-subscriptionRouter.get('/subscribe', (req, res) => res.send({title: "Get all subscriptions"}));
+subscriptionRouter.get('/', (req, res) => res.send({title: "GET all subscriptions"}));
+subscriptionRouter.get('/:id', (req, res) => res.send({title: "Get subscription detail"}));
+subscriptionRouter.post('/', (req, res) => res.send({title: "CREATE subscription"}));
+subscriptionRouter.put('/:id', (req, res) => res.send({title: "UPDATE subscription  "}));
+subscriptionRouter.delete('/:id', (req, res) => res.send({title: "DELETE subscription"}));
+subscriptionRouter.get('/user/:id', (req, res) => res.send({title: "GET all user subscriptions"}));
+subscriptionRouter.put('/:id/cancel', (req, res) => res.send({title: "CREATE all subscriptions"}));
+subscriptionRouter.get('/upcoming-renewals', (req, res) => res.send({title: "GET upcoming renewals"}));
 
 export default subscriptionRouter;
