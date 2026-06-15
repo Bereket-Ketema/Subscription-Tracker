2fd5efdf01f339695d595fee64b51334b39c5fec
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-15 17:17:32 +0300

    the get user and users have been added form the controller

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import {Router} from 'express';
+import { getUser, getUsers } from '../controllers/user.controller.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) =>  res.send({title: "Fetch all users"}));
+userRouter.get('/', (req, res) =>  getUsers);
 
-userRouter.get('/:id', (req, res) =>  res.send({title: "GET user details"}));
+userRouter.get('/:id', (req, res) =>  getUser);
 
 userRouter.post('/', (req, res) =>  res.send({title: "Create new user"}));
 userRouter.put('/:id', (req, res) =>  res.send({title: "Update all users"}));
