90da16e470f43077251491cc8cf7408d1e039197
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-15 10:14:03 +0300

    the error handler middleware the mongo db error handler added on it

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -5,8 +5,33 @@ const errorMiddleware = (err, req, res, next) => {
     error.message = err.message
     console.error(err);
 
-    //type of error detection it may be (Mongoose bad ObjectId)
+    //type of error detection it may be 
+
+    // Mongoose bad ObjectId
+    if (err.name === 'CastError'){
+      const message = 'Resource not found';
+      error = new Error(message);
+      error.StatusCode = 404;
+    }
+
+    //Mongoose duplicate key
+    if (err.code === 11000){
+       const message = 'Duplicate field value entered';
+       error = new Error(message);
+       error.StatusCode = 400;
+    }
+
+    //Mongoose validation console.error();
+    if (err.name === 'ValidationError'){
+      const message = Object.values(err.errors).map( val => val.message);
+      error = new Error(message.join(', '));
+      error.StatusCode = 400;
+    }
+
+    res.status(error.StatusCode || 500).json({success: false, error: error.message || 'Server Error'});
   } catch(error){
     next(error);
   }
 }
+
+export default errorMiddleware;
