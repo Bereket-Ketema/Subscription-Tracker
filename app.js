cc2618db22300af079df44cd4ca737dc4336f1d9
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-15 10:15:12 +0300

    the json, urlencoded and cookieparser middleware have been added on it

--- app.js
+++ app.js
@@ -4,13 +4,22 @@ import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 
 const app = express();
 
+//middleware
+app.use(express.json());//able to handle json data send in request or api calls
+app.use(express.urlencoded({extended: false}));// to process the form data sent by html forms in a simple format
+app.use(cookieParser());// read cookies form incoming request able app to store user data
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);// we have created our own error cases
 
 app.get('/', (req, res) => {
   res.send("Hello world to this api");
