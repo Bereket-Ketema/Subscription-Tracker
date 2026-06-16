ce123fd4c69d971c5eb5a255758f642f2c7c25f1
Bereket-Ketema <bereketketema866@gmail.com>
2026-06-16 07:03:06 +0300

    the authorizeation of the controller have ben added on the get router

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,13 @@
 import {Router} from 'express';
+
 import { getUser, getUsers } from '../controllers/user.controller.js';
+import authorize from '../middlewares/auth.middleware.js';
 
 const userRouter = Router();
 
-userRouter.get('/', (req, res) =>  getUsers);
+userRouter.get('/', getUsers);
 
-userRouter.get('/:id', (req, res) =>  getUser);
+userRouter.get('/:id', authorize, getUser);
 
 userRouter.post('/', (req, res) =>  res.send({title: "Create new user"}));
 userRouter.put('/:id', (req, res) =>  res.send({title: "Update all users"}));
